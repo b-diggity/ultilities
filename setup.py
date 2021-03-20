9a29f94c67487bebb1292c5256c08f8d533873c1
bdiggity <bdiggity@outlook.com>
2021-03-19 21:20:19 -0500

    added setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+# requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
+
+setup(
+    name="utilities",
+    version="0.0.1",
+    author="b-diggity",
+    description="Utilities to include in Python scripts",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/b-diggity/utilities",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT",
+    ],
+)
